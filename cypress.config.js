e68fdc2c8ba93b9988620d2b0fe16238a8795756
Michael Perju <michael@this.one>
2023-07-14 18:40:38 +0100

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,60 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require("cypress")
+const { MongoClient } = require("mongodb")
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+
+      on('task', {
+        async 'db:drop'() {
+
+          const connectionString = "mongodb+srv://gro-dev:vYBUGp6MNqkGVNfY@gro.d2su6ry.mongodb.net/thisone-dev?retryWrites=true&w=majority"
+          const dbName = "thisone-dev"
+
+          console.log(`Dropping database: ${dbName}`)
+          const client = new MongoClient(connectionString, { useUnifiedTopology: true })
+
+          try {
+            await client.connect()
+            const adminDb = client.db("admin")
+            const dropDbCommand = { dropDatabase: 1 }
+            await adminDb.command(dropDbCommand)
+            console.log(`Database ${dbName} dropped successfully!`)
+          } catch (error) {
+            console.log(`Error dropping database: ${error}`)
+          } finally {
+            await client.close()
+          }
+
+          return true
+        },
+        async 'db:seed'() {
+
+          // const connectionString = "mongodb+srv://gro-dev:vYBUGp6MNqkGVNfY@gro.d2su6ry.mongodb.net/thisone-dev?retryWrites=true&w=majority"
+          // const dbName = "thisone-dev"
+
+          // console.log(`Dropping database: ${dbName}`)
+          // const client = new MongoClient(connectionString, { useUnifiedTopology: true })
+
+          // try {
+          //   await client.connect()
+          //   const adminDb = client.db("admin")
+          //   const dropDbCommand = { dropDatabase: 1 }
+          //   await adminDb.command(dropDbCommand)
+          //   console.log(`Database ${dbName} dropped successfully!`)
+          // } catch (error) {
+          //   console.log(`Error dropping database: ${error}`)
+          // } finally {
+          //   await client.close()
+          // }
+
+          return true
+        },
+
+
+
+
+      })
     },
   },
-});
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,22 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// const { MongoClient } = require("mongodb")
+
+// const connectionString = "mongodb+srv://gro-dev:vYBUGp6MNqkGVNfY@gro.d2su6ry.mongodb.net/thisone-dev?retryWrites=true&w=majority"
+// const dbName = "thisone-dev"
+
+// Cypress.Commands.add("dropDatabase", async () => {
+//   cy.log(`Dropping database: ${dbName}`);
+
+//   const client = new MongoClient(connectionString, { useUnifiedTopology: true });
+
+//   try {
+//     await client.connect();
+//     const adminDb = client.db("admin");
+//     const dropDbCommand = { dropDatabase: 1 };
+//     await adminDb.command(dropDbCommand);
+//     cy.log(`Database ${dbName} dropped successfully!`);
+//   } catch (error) {
+//     cy.log(`Error dropping database: ${error}`);
+//   } finally {
+//     await client.close();
+//   }
+// });
